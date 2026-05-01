49a75f44219078f7f674314ab41a058ff3541703
Alexandr <alexandr.mikhaylov.88@mail.ru>
2026-05-01 07:22:32 +0300

    MyFirstApp

---
+++ src/FitstApp/Program.java
@@ -0,0 +1,18 @@
+package FitstApp;
+
+public class Program {
+    static void main() {
+        System.out.println("Меня зовут Александр.");
+        System.out.println("я из Краснодара!" + "\nЯ начинаю изучать Java");
+        System.out.println();
+        System.out.println("Hello world");
+        System.out.println("Александр, мне 37 лет, я учу Java");
+        System.out.println("План на неделю:");
+        System.out.println("День 1. Установка и первый запуск");
+        System.out.println("День 2. Переменные, типы данных, ввод и вывод");
+        System.out.println("День 3. Условные конструкции");
+        System.out.println("День 4. Циклы");
+        System.out.println("День 5. Методы");
+        System.out.println("День 7. Разбор и закрепление");
+    }
+}
