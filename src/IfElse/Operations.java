848e09661f62e19756d0bdd5deb4cb978dcb8d20
Alexandr <alexandr.mikhaylov.88@mail.ru>
2026-05-11 16:18:17 +0300

    MyFirstApp

---
+++ src/IfElse/Operations.java
@@ -0,0 +1,36 @@
+package IfElse;
+import java.util.Scanner;
+
+public class Operations {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int number = Integer.parseInt(sc.nextLine());
+        if (number % 2 == 0) {
+            System.out.println("Четное");
+        } else {
+            System.out.println("Не четное");
+        }
+
+        int age = 17;
+        if (age < 18)
+            System.out.println("Вы еще ребенок");
+        else
+            System.out.println("Вы уже взрослый");
+
+        System.out.println("Введите любое число");
+        int num = sc.nextInt();
+        if (num >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+
+        int num1 = 23;
+        if (num1 > 0 && num1 < 50) {
+            System.out.println("Входит в диапозон");
+        } else {
+            System.out.println("Не входит в диапозон");
+        }
+    }
+}
